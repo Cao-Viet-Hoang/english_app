3493120d286e7c3f153e1babb64a2bdf2f821dc9
Cao-Viet-Hoang <cah8hc@bosch.com>
2025-12-13 17:05:02 +0700

    feat: Add games module with navigation and UI for vocabulary games

--- js/app.js
+++ js/app.js
@@ -33,6 +33,7 @@ function initializeApp() {
   setupModalListeners();
   setupBottomSheetListeners();
   setupUtilityListeners();
+  setupGamesListeners();
   
   console.log('App initialized successfully!');
 }
